e402429cd8ff02076853ee0b00ac62e5255a5472
RezuanD <dzibov.fortech@gmail.com>
2024-02-26 16:55:12 +0300

    describe getPing controller in swagger

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,11 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('General')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/ping')
+  @ApiOkResponse({
+    content: {
+      'application/json': {
+        example: [
+          {
+            message: 'pong',
+          },
+        ],
+      },
+    },
+  })
   getPing(): { message: string } {
     return this.appService.getPing();
   }
