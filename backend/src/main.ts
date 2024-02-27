9c6141367259301c7e8a51326bc0ab902b19a158
Rezuan Dzibov <dzibov.fortech@gmail.com>
2024-02-27 09:41:09 +0300

    remove unnecessery generic use

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,13 +8,13 @@ async function bootstrap() {
   const config = app.get(ConfigService);
 
   const swaggerConfig = new DocumentBuilder()
-    .setTitle(config.get<string>('PROJECT_NAME'))
-    .setDescription(config.get<string>('PROJECT_DESCRIPTION'))
-    .setVersion(config.get<string>('PROJECT_VERSION'))
+    .setTitle(config.get('PROJECT_NAME'))
+    .setDescription(config.get('PROJECT_DESCRIPTION'))
+    .setVersion(config.get('PROJECT_VERSION'))
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(Number(config.get<string>('BACKEND_API_PORT')));
+  await app.listen(Number(config.get('BACKEND_API_PORT')));
 }
 bootstrap();
