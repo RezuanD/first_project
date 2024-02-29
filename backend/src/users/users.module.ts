8a06462ee1ba9a1311c97057f7a1939c85a6ed7f
Rezuan Dzibov <dzibov.fortech@gmail.com>
2024-02-29 16:52:25 +0300

    add UsersHelper to providers array in UsersModule

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './user.contoller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { UsersHelper } from './helpers/users.helpers';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersHelper],
 })
 export class UsersModule {}
