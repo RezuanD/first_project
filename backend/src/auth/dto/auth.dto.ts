07823df216d30c587ca27d77899725adfbfcdeb2
Rezuan Dzibov <dzibov.fortech@gmail.com>
2024-03-01 17:43:18 +0300

    fix SignInDto

--- backend/src/auth/dto/auth.dto.ts
+++ backend/src/auth/dto/auth.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty, PickType } from '@nestjs/swagger';
 import { IsString, IsStrongPassword } from 'class-validator';
 import { User } from '@/users/user.entity';
 
-export class CreateUserDto extends PickType(User, ['username']) {
+export class SignInDto extends PickType(User, ['username']) {
   @ApiProperty()
   @IsString()
   @IsStrongPassword({
