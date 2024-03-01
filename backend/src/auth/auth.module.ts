99fd3c70c22cc1adc02b5d9d5566b1b0633efbe8
Rezuan Dzibov <dzibov.fortech@gmail.com>
2024-03-01 17:45:41 +0300

    fix AuthModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -7,7 +7,6 @@ import { jwtConstants } from './constants';
 import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
 import { UsersHelper } from '@/users/helpers/users.helpers';
-<<<<<<< HEAD
 import { User } from '@/users/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
@@ -16,12 +15,6 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     TypeOrmModule.forFeature([User]),
     ConfigModule,
-=======
-import { UserService } from '@/users/users.service';
-
-@Module({
-  imports: [
->>>>>>> ce5b1fabd9c0e468a69a6e756bdb9dc42dbcc4a1
     UserModule,
     PassportModule,
     JwtModule.register({
@@ -29,11 +22,7 @@ import { UserService } from '@/users/users.service';
       signOptions: { expiresIn: '60s' },
     }),
   ],
-<<<<<<< HEAD
   providers: [AuthService, LocalStrategy, UsersHelper],
-=======
-  providers: [AuthService, LocalStrategy, UsersHelper, UserService],
->>>>>>> ce5b1fabd9c0e468a69a6e756bdb9dc42dbcc4a1
   controllers: [AuthController],
 })
 export class AuthModule {}
