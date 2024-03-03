a090f80936a6334089c5ded9cca6bb7252441b0b
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2024-03-03 23:17:59 +0300

    fix import order

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { ClassSerializerInterceptor } from '@nestjs/common';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
