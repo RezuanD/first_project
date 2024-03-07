84fb1bde12841af3835769ec2647c7d23c00e1ca
Rezuan Dzibov <dzibov.fortech@gmail.com>
2024-03-07 15:04:48 +0300

    remove ArticleHelpers

--- backend/src/blog/blog.module.ts
+++ backend/src/blog/blog.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Article } from '@/blog/entities/article.entity';
 import { ArticleService } from '@/blog/article.service';
-import { ArticleHelpers } from '@/blog/article.helpers';
 import { ArticleController } from '@/blog/blog.controller';
 import { UserHelpers } from '@/users/helpers/users.helpers';
 import { User } from '@/users/user.entity';
@@ -11,6 +10,6 @@ import { User } from '@/users/user.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([Article, User]), ConfigModule],
   controllers: [ArticleController],
-  providers: [ArticleService, UserHelpers, ArticleHelpers],
+  providers: [ArticleService, UserHelpers],
 })
 export class BlogModule {}
