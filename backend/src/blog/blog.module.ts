e68784745a7fbeafd31e2eb5adcbe39c2fe22479
Rezuan Dzibov <dzibov.fortech@gmail.com>
2024-03-07 15:53:52 +0300

    unregister UserHelpers in BlogModule

--- backend/src/blog/blog.module.ts
+++ backend/src/blog/blog.module.ts
@@ -4,12 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Article } from '@/blog/entities/article.entity';
 import { ArticleService } from '@/blog/article.service';
 import { ArticleController } from '@/blog/blog.controller';
-import { UserHelpers } from '@/users/helpers/users.helpers';
 import { User } from '@/users/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Article, User]), ConfigModule],
   controllers: [ArticleController],
-  providers: [ArticleService, UserHelpers],
+  providers: [ArticleService],
 })
 export class BlogModule {}
