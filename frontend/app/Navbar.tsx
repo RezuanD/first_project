8a7f05cd33e6d4479d25d550ffca9667af765951
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2024-03-10 15:51:00 +0300

    add article add button

--- frontend/app/Navbar.tsx
+++ frontend/app/Navbar.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import Link from "next/link";
 import { useAuth } from "./auth.context";
@@ -10,20 +10,20 @@ export default function Navbar() {
   const router = useRouter();
 
   const handleLogout = async () => {
-    await fetch('http://127.0.0.1:8000/auth/logout', {
-      method: 'POST',
+    await fetch("http://127.0.0.1:8000/auth/logout", {
+      method: "POST",
       headers: {
-        'Authorization': `Bearer ${localStorage.getItem('access_token')}`,
-        'Content-Type': 'application/json'
-      }
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        "Content-Type": "application/json",
+      },
     });
 
-    localStorage.removeItem('access_token');
+    localStorage.removeItem("access_token");
 
     setIsLoggedIn(false);
 
-    router.push('/');  
-  }
+    router.push("/");
+  };
 
   return (
     <div className="flex flex-row justify-between w-[90%] h-20 rounded-2xl mt-8 p-4 border-2 shadow-lg shadow-primary">
@@ -32,11 +32,16 @@ export default function Navbar() {
           <h1 className="text-2xl font-bold italic">Bloggers of The World</h1>
         </Link>
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
         {isLoggedIn ? (
-          <Link href="/">
-            <Button onClick={handleLogout}>Logout</Button>
-          </Link>
+          <div className="flex gap-2">
+            <Link href="/article/add">
+              <Button>Add Article +</Button>
+            </Link>
+            <Link href="/">
+              <Button onClick={handleLogout}>Logout</Button>
+            </Link>
+          </div>
         ) : (
           <Link href="/login">
             <Button>Login</Button>
